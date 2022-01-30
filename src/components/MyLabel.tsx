0640ea374831ea22428b41474899e5d6512c8c41
Programito <Programito@gmail.com>
2022-01-30 17:03:01 +0100

    storybook mylabel

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,39 @@
+import './myLabel.css'
+
+export interface MyLabelProps {
+    /**
+     * Este el mensaje a mostrar en la etiqueta
+     */
+    label?: string;
+    /**
+     * Este es el tamaño de la etiqueta
+     */
+    size?: 'normal' | 'h1' | 'h2' | 'h3'
+    /**
+     * Este quiere todo capitalizado
+     */
+    allCaps?: boolean
+    /**
+     * Colores basico del color
+     */
+    color?: 'primary' | 'secondary' | 'tertiary'
+    /**
+     * Colores personalizado de la fuente
+     */
+    fontColor?: string
+
+}
+
+export const MyLabel = ({
+        allCaps = false,
+        color = 'primary',
+        label = 'No label',
+        size = 'normal',
+        fontColor
+    }: MyLabelProps) => {
+    return (
+        <span className={`label ${size} text-${color}`} style={{color: fontColor}}>
+            {allCaps? label.toUpperCase() : label}
+        </span>
+    )
+}
