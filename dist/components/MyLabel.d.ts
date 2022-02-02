0ad35f387943d430d66c4604c5d8e6136315b14b
Programito <Programito@gmail.com>
2022-02-02 19:24:13 +0100

    feat: agregamos semantic-versioning

---
+++ dist/components/MyLabel.d.ts
@@ -0,0 +1,29 @@
+/// <reference types="react" />
+import './myLabel.css';
+export interface MyLabelProps {
+    /**
+     * Este el mensaje a mostrar en la etiqueta
+     */
+    label?: string;
+    /**
+     * Este es el tamaño de la etiqueta
+     */
+    size?: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * Este quiere todo capitalizado
+     */
+    allCaps?: boolean;
+    /**
+     * Colores basico del color
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * Colores personalizado de la fuente
+     */
+    fontColor?: string;
+    /**
+     *
+     */
+    backgroundColor?: string;
+}
+export declare const MyLabel: ({ allCaps, color, label, size, fontColor, backgroundColor }: MyLabelProps) => JSX.Element;
